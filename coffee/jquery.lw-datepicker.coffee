ce04ad3a24d39289d9029599b0fead541a0057f5
Maxim Zhukov <zhkv.mxm@gmail.com>
2011-11-16 16:52:11 +0700

    Added rendering of first week.

--- coffee/jquery.lw-datepicker.coffee
+++ coffee/jquery.lw-datepicker.coffee
@@ -1,7 +1,7 @@
 # References jQuery
 $ = jQuery
 
-dowNames = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
+dowNames = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat']
 monthNames = ['January', 'February', 'March', 'April', 'May', 'June',
     'July', 'August', 'September', 'October', 'November', 'December']
 
@@ -12,22 +12,30 @@ settings =
 
 class LightweightDatepicker
 
-  currentDate: new Date
+  activeDate: null
 
   constructor: (settings) ->
     @settings = settings
+
+    @currentDate = new Date
+    @todayDate = new Date
+
+    first = @settings.firstDayOfTheWeek.toLowerCase()
+    @firstDowIndex = 0
+    for name in dowNames
+      if name is first then break
+      @firstDowIndex++
 
     @wrapper = $ '<div class="lw-dp"/>'
     @toolbar = $('<div class="lw-dp-toolbar"/>').appendTo @wrapper
     @previous = $('<div class="lw-dp-previous">◄</div>').appendTo @toolbar
     @next = $('<div class="lw-dp-next">►</div>').appendTo @toolbar
     @month = $('<div class="lw-dp-month"/>').appendTo @toolbar
+    @renderDows().appendTo @wrapper
     @days = $('<div />').appendTo @wrapper
     
     @next.bind 'click', => @onNextClick()
     @previous.bind 'click', => @onPreviousClick()
-
-    @renderDows().appendTo @wrapper
     @updateMonth()
 
     @wrapper.appendTo document.body
@@ -45,7 +53,41 @@ class LightweightDatepicker
     @month.html monthNames[@currentDate.getMonth()] + ', ' + @currentDate.getFullYear()
 
     # Updating dates
+    cd = @currentDate
+    dayIndex = 0
+    firstDayDow = (new Date(cd.getFullYear(), cd.getMonth(), 1)).getDay()
+    adjustedFirstDow = firstDayDow - @firstDowIndex
+    if adjustedFirstDow < 0 then adjustedFirstDow = 7 + adjustedFirstDow
+    daysInPreviousMonth = (new Date(cd.getFullYear(), cd.getMonth(), 0)).getDate()
+    daysInFirstWeek = (7 - firstDayDow + @firstDowIndex) % 7
+    if daysInFirstWeek is 0 then daysInFirstWeek = 7
+    remainingDays = (new Date(cd.getFullYear(), cd.getMonth() + 1, 0)).getDate()
+    remainingDays -= daysInFirstWeek
+    startDatePreviousMonth = daysInPreviousMonth - (6 - daysInFirstWeek)
+
+    renderDay = (day)->
+      classes = []
+      classAttribute = ''
+      dow = (dayIndex + firstDayDow) % 7
+      if dow is 0 or dow is 6 # weekends
+        classes.push 'lw-dp-weekend'
+      if ((dayIndex + adjustedFirstDow) % 7) is 6
+        classes.push 'lw-dp-week-last-column'
+      if classes.length
+        classAttribute = " class='#{classes.join " "}'"
+      dayIndex++
+      """<li#{classAttribute}>#{day}</li>"""
     
+    html = '<ul class="lw-dp-week lw-dp-firstweek">'
+    if startDatePreviousMonth <= daysInPreviousMonth
+      for day in [startDatePreviousMonth..daysInPreviousMonth]
+        html += """<li class="lw-dp-neighbour-month-day">#{day}</li>"""
+    for day in [1..daysInFirstWeek]
+      html += renderDay day
+    html += '</ul>'
+    @days.html html
+
+
 
   renderDows: ->
     first = @settings.firstDayOfTheWeek.toLowerCase()
--- js/jquery.lw-datepicker.js
+++ js/jquery.lw-datepicker.js
@@ -2,7 +2,7 @@
   var $, LightweightDatepicker, dowNames, monthNames, settings;
   var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
   $ = jQuery;
-  dowNames = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+  dowNames = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
   monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   settings = {
     multiple: false,
@@ -10,14 +10,27 @@
     dateFormat: 'yyyy.mm.dd'
   };
   LightweightDatepicker = (function() {
-    LightweightDatepicker.prototype.currentDate = new Date;
+    LightweightDatepicker.prototype.activeDate = null;
     function LightweightDatepicker(settings) {
+      var first, name, _i, _len;
       this.settings = settings;
+      this.currentDate = new Date;
+      this.todayDate = new Date;
+      first = this.settings.firstDayOfTheWeek.toLowerCase();
+      this.firstDowIndex = 0;
+      for (_i = 0, _len = dowNames.length; _i < _len; _i++) {
+        name = dowNames[_i];
+        if (name === first) {
+          break;
+        }
+        this.firstDowIndex++;
+      }
       this.wrapper = $('<div class="lw-dp"/>');
       this.toolbar = $('<div class="lw-dp-toolbar"/>').appendTo(this.wrapper);
       this.previous = $('<div class="lw-dp-previous">◄</div>').appendTo(this.toolbar);
       this.next = $('<div class="lw-dp-next">►</div>').appendTo(this.toolbar);
       this.month = $('<div class="lw-dp-month"/>').appendTo(this.toolbar);
+      this.renderDows().appendTo(this.wrapper);
       this.days = $('<div />').appendTo(this.wrapper);
       this.next.bind('click', __bind(function() {
         return this.onNextClick();
@@ -25,7 +38,6 @@
       this.previous.bind('click', __bind(function() {
         return this.onPreviousClick();
       }, this));
-      this.renderDows().appendTo(this.wrapper);
       this.updateMonth();
       this.wrapper.appendTo(document.body);
     }
@@ -36,11 +48,55 @@
       return this.updateMonth(-1);
     };
     LightweightDatepicker.prototype.updateMonth = function(diff) {
+      var adjustedFirstDow, cd, day, dayIndex, daysInFirstWeek, daysInPreviousMonth, firstDayDow, html, remainingDays, renderDay, startDatePreviousMonth;
       if (diff == null) {
         diff = 0;
       }
       this.currentDate.setMonth(this.currentDate.getMonth() + diff);
-      return this.month.html(monthNames[this.currentDate.getMonth()] + ', ' + this.currentDate.getFullYear());
+      this.month.html(monthNames[this.currentDate.getMonth()] + ', ' + this.currentDate.getFullYear());
+      cd = this.currentDate;
+      dayIndex = 0;
+      firstDayDow = (new Date(cd.getFullYear(), cd.getMonth(), 1)).getDay();
+      adjustedFirstDow = firstDayDow - this.firstDowIndex;
+      if (adjustedFirstDow < 0) {
+        adjustedFirstDow = 7 + adjustedFirstDow;
+      }
+      daysInPreviousMonth = (new Date(cd.getFullYear(), cd.getMonth(), 0)).getDate();
+      daysInFirstWeek = (7 - firstDayDow + this.firstDowIndex) % 7;
+      if (daysInFirstWeek === 0) {
+        daysInFirstWeek = 7;
+      }
+      remainingDays = (new Date(cd.getFullYear(), cd.getMonth() + 1, 0)).getDate();
+      remainingDays -= daysInFirstWeek;
+      startDatePreviousMonth = daysInPreviousMonth - (6 - daysInFirstWeek);
+      renderDay = function(day) {
+        var classAttribute, classes, dow;
+        classes = [];
+        classAttribute = '';
+        dow = (dayIndex + firstDayDow) % 7;
+        if (dow === 0 || dow === 6) {
+          classes.push('lw-dp-weekend');
+        }
+        if (((dayIndex + adjustedFirstDow) % 7) === 6) {
+          classes.push('lw-dp-week-last-column');
+        }
+        if (classes.length) {
+          classAttribute = " class='" + (classes.join(" ")) + "'";
+        }
+        dayIndex++;
+        return "<li" + classAttribute + ">" + day + "</li>";
+      };
+      html = '<ul class="lw-dp-week lw-dp-firstweek">';
+      if (startDatePreviousMonth <= daysInPreviousMonth) {
+        for (day = startDatePreviousMonth; startDatePreviousMonth <= daysInPreviousMonth ? day <= daysInPreviousMonth : day >= daysInPreviousMonth; startDatePreviousMonth <= daysInPreviousMonth ? day++ : day--) {
+          html += "<li class=\"lw-dp-neighbour-month-day\">" + day + "</li>";
+        }
+      }
+      for (day = 1; 1 <= daysInFirstWeek ? day <= daysInFirstWeek : day >= daysInFirstWeek; 1 <= daysInFirstWeek ? day++ : day--) {
+        html += renderDay(day);
+      }
+      html += '</ul>';
+      return this.days.html(html);
     };
     LightweightDatepicker.prototype.renderDows = function() {
       var day, first, found, html, name, temp, _i, _len;
